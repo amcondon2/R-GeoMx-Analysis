5afb3a5251bc5761d45141d868fb907e2931da96
amcondon2 <116680471+amcondon2@users.noreply.github.com>
2024-03-25 13:15:30 -0700

    initial pathway analysis code

--- matlab scripts/roiToMeans.m
+++ matlab scripts/roiToMeans.m
@@ -14,9 +14,9 @@ snrLims = [35, 90];
 %% Generate Output Table
 
 imageColumnNames = roiArray{1}.imageColumnNames;
-columnNames = ["Case", "Run", "Sex", "toggleChannel"];
+columnNames = ["Case", "Run", "Sex", "Group", "toggleChannel"];
 columnNames = [columnNames, imageColumnNames];
-columnTypes = ["double", "double", "categorical", "double", repmat("cell", 1, length(imageColumnNames))]; %predefine column names and types
+columnTypes = ["double", "double", "categorical", "categorical", "double", repmat("cell", 1, length(imageColumnNames))]; %predefine column names and types
 
 outTable = table('Size',[0,length(columnNames)], 'VariableTypes',columnTypes, 'VariableNames',columnNames); %create empty table
 
@@ -32,6 +32,7 @@ for roiIdx = 1:numRoi
     newRow.Case = roiArray{roiIdx}.originCase;
     newRow.Run = roiArray{roiIdx}.originRun;
     newRow.Sex = unique(roiArray{roiIdx}.imageTable.sex);
+    newRow.Group = roiArray{roiIdx}.imageTable.treatment;
     newRow.toggleChannel = unique(roiArray{roiIdx}.imageTable.toggleChannel);
     
     for imIdx = 1:length(imageColumnNames)
@@ -46,8 +47,8 @@ outTable.SNR2 = [];
 outTable = sortrows(outTable, 1);
 
 
-columnNames = ["sample", "Sex", "LT1mean", "LT2mean", "LT3mean", "LT1med", "LT2med", "LT3med", "I1mean", "I2mean", "I3mean", "I1med", "I2med", "I3med", "IR13mean", "IR23mean"];
-columnTypes = ["double", "categorical", repmat("double", 1, length(columnNames)-2)]; %predefine column names and types
+columnNames = ["sample", "Sex", "Group", "LT1mean", "LT2mean", "LT3mean", "LT1med", "LT2med", "LT3med", "I1mean", "I2mean", "I3mean", "I1med", "I2med", "I3med", "IR13mean", "IR23mean"];
+columnTypes = ["double", "categorical", "categorical" ,repmat("double", 1, length(columnNames)-3)]; %predefine column names and types
 
 summaryTable = table('Size',[0,length(columnNames)], 'VariableTypes',columnTypes, 'VariableNames',columnNames); %create empty table
 caseNums = unique(outTable.Case);
@@ -67,6 +68,12 @@ for caseIdx = 1:height(caseNums)
     newRow = [];
     newRow.sample = caseNums(caseIdx);
     newRow.Sex = unique(outTable(outTable.Case == caseNums(caseIdx), :).Sex);
+    
+    if outTable(outTable.Case == caseNums(caseIdx), :).Group == "strep"
+        newRow.Group = "test";
+    else
+        newRow.Group = "control";
+    end
 
     newRow.LT1mean = mean(ch1LTmap, 'omitnan');
     newRow.LT2mean = mean(ch2LTmap, 'omitnan');
@@ -92,4 +99,4 @@ for caseIdx = 1:height(caseNums)
     summaryTable = [summaryTable; struct2table(newRow, 'AsArray',true)];
 end
 
-writetable(summaryTable, "roiSummaryTable.csv");
+writetable(summaryTable, "roiSummaryTable2.csv");
